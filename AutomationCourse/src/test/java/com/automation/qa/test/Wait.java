4a6f631c30bc01acbe933cbe1fd8f7ada9482028
DayaShanka <daya.shankar@thinkexam.com>
2024-10-14 17:34:02 +0530

    wait

---
+++ AutomationCourse/src/test/java/com/automation/qa/test/Wait.java
@@ -0,0 +1,27 @@
+package com.automation.qa.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Wait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://sso.teachable.com/secure/9521/identity/login/password");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Declaration
+		WebElement waitElement = mywait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@alt='Rahul Shetty Academy']")));
+		System.out.println(waitElement.isDisplayed());
+		// System.out.println(driver.findElement(By.xpath("//img[@alt='Rahul Shetty
+		// Academy']")).isDisplayed());
+	}
+
+}
