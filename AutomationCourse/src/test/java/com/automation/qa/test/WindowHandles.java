436077fee80649bf9614eed7e268041bf10e7ec6
DayaShanka <daya.shankar@thinkexam.com>
2024-10-15 16:56:33 +0530

    done

--- AutomationCourse/src/test/java/com/automation/qa/test/WindowHandles.java
+++ AutomationCourse/src/test/java/com/automation/qa/test/WindowHandles.java
@@ -17,14 +17,14 @@ public class WindowHandles {
 		// driver.get("https://google.com/");
 //		String windowid = driver.getWindowHandle(); // for single window id- 8098ED2B04660DB1ED6641E13B2019B4
 //		System.out.println(windowid);
-		driver.findElement(By.xpath("//a[text()='Postman Support Center']")).click();
+		driver.findElement(By.xpath("//a[text()='Postman Support Center']")).click(); // open another window
 		Set<String> multiwindowids = driver.getWindowHandles();
 		System.out.println("All window IDS " + multiwindowids); // printing ids of both window
-		List<String> windowIds = new ArrayList<>(multiwindowids);
-		String parentwinid = windowIds.get(0); //
+		List<String> windowIds = new ArrayList<>(multiwindowids); // convert into ArrayList
+		String parentwinid = windowIds.get(0); // storing with the help of index
 		String childwinid = windowIds.get(1);
 		System.out.println(parentwinid + " ," + childwinid);
-		driver.switchTo().window(childwinid);
+		driver.switchTo().window(childwinid); // stand on child window
 		System.out.println(driver.findElement(By.xpath("//h1[text()='Postman support center']")).getText());
 		System.out.println(driver.getCurrentUrl());
 		driver.switchTo().window(parentwinid);
